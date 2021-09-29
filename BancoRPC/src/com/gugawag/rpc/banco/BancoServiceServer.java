41505ae82c8d14dfafdc11eb71a8bf9deacbf95e
RafaelMarinhoo <marinhor978@gmail.com>
2021-09-29 18:41:29 -0300

    exercicio-rpc

---
+++ BancoRPC/src/com/gugawag/rpc/banco/BancoServiceServer.java
@@ -0,0 +1,36 @@
+package com.gugawag.rpc.banco;
+
+import com.gugawag.rpc.banco.model.ContaBanco;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BancoServiceServer extends UnicastRemoteObject implements BancoServiceIF {
+
+    private Map<String, Double> saldoContas;
+
+    private List<ContaBanco> contas;
+    public BancoServiceServer() throws RemoteException {
+        saldoContas = new HashMap<>();
+        saldoContas.put("1", 100.0);
+        saldoContas.put("2", 156.0);
+        saldoContas.put("3", 950.0);
+    }
+
+    @Override
+    public double saldo(String conta) throws RemoteException {
+        return saldoContas.get(conta);
+    }
+
+    @Override
+    public int quantidadeContas() throws RemoteException {
+        return saldoContas.size();
+    }
+    @Override
+    public void adicionarConta(String numero, Double saldo) throws RemoteException {
+
+        contas.add(new ContaBanco(numero, saldo));
+    }
+}
