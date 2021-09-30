0531d18901e9ae6b4af7cf0c5e3e261771320863
RafaelMarinhoo <marinhor978@gmail.com>
2021-09-29 21:22:25 -0300

    exercicioEJB

---
+++ pratica-ejb/src/main/java/com/gugawag/pdist/ejbs/MensagemService.java
@@ -0,0 +1,28 @@
+package com.gugawag.pdist.ejbs;
+
+import com.gugawag.pdist.model.Mensagem;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless(name = "mensagemService")
+public class MensagemService {
+
+    @EJB
+    private MensagemDAO mensagemDAO;
+
+    public List<Mensagem> listar() {
+        return mensagemDAO.listar();
+    }
+
+    public void inserir(long id, String texto) {
+        Mensagem newMensagem = new Mensagem(id, texto);
+        mensagemDAO.inserir(newMensagem);
+    }
+
+    public Mensagem pesquisarPorId(long id) {
+        return mensagemDAO.pesquisarPorId(id);
+    }
+
+}
